c9405447d144ac0e3b3370e1e357b211c0993471
Antonin Hildebrand <antonin@hildebrand.cz>
2010-04-18 19:37:04 +0200

    TotalFinder.osax - initial commit as of TotalFinder 0.8.3

---
+++ TotalFinderInjector.m
@@ -0,0 +1,78 @@
+#import "TFStandardVersionComparator.h"
+
+#define TOTALFINDER_PLUGIN_PATH @"/Applications/TotalFinder.app/Contents/Resources/TotalFinder.bundle"
+#define FINDER_MIN_ALLOWED_VERSION @"10.6"
+#define FINDER_MAX_ALLOWED_VERSION @"10.6.4"
+
+// SIMBL-compatible interface
+@interface TotalFinderPlugin: NSObject { 
+}
+- (void) install;
+@end
+
+static bool alreadyLoaded = false;
+
+OSErr HandleInitEvent(const AppleEvent *ev, AppleEvent *reply, long refcon) {
+    NSLog(@"TotalFinderInjector: got init request");
+    if (alreadyLoaded) {
+        NSLog(@"TotalFinderInjector: TotalFinder has been already loaded. Ignoring this request.");
+        return noErr;
+    }
+    @try {
+        NSBundle* finderBundle = [NSBundle mainBundle];
+        if (!finderBundle) {
+            NSLog(@"TotalFinderInjector: Unable to locate main Finder bundle!");
+            return 4;
+        }
+        
+        NSString* finderVersion = [finderBundle objectForInfoDictionaryKey:@"CFBundleVersion"];
+        if (!finderVersion) {
+            NSLog(@"TotalFinderInjector: Unable to determine Finder version!");
+            return 5;
+        }
+        
+        // future compatibility check
+        NSString* supressKey = @"TotalFinderSuppressFinderVersionCheck";
+        NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+        if (![defaults boolForKey:supressKey]) {
+            TFStandardVersionComparator* comparator = [TFStandardVersionComparator defaultComparator];
+            if (([comparator compareVersion:finderVersion toVersion:FINDER_MAX_ALLOWED_VERSION]==NSOrderedDescending) || 
+                ([comparator compareVersion:finderVersion toVersion:FINDER_MIN_ALLOWED_VERSION]==NSOrderedAscending)) {
+
+                NSAlert* alert = [NSAlert new];
+                [alert setMessageText: [NSString stringWithFormat:@"You have Finder version %@", finderVersion]];
+                [alert setInformativeText: [NSString stringWithFormat:@"But TotalFinder was properly tested only with Finder versions in range %@ - %@\n\nYou have probably updated your system and Finder version got bumped by Apple developers.\n\nYou may expect a new TotalFinder release soon.", FINDER_MIN_ALLOWED_VERSION, FINDER_MAX_ALLOWED_VERSION]];
+                [alert setShowsSuppressionButton:YES];
+                [alert addButtonWithTitle:@"Launch TotalFinder anyway"];
+                [alert addButtonWithTitle:@"Cancel"];
+                NSInteger res = [alert runModal];
+                if ([[alert suppressionButton] state] == NSOnState) {
+                    [defaults setBool:YES forKey:supressKey];
+                }
+                if (res!=NSAlertFirstButtonReturn) { // cancel
+                    return noErr;
+                }
+            }
+        }
+        
+        NSBundle* pluginBundle = [NSBundle bundleWithPath:TOTALFINDER_PLUGIN_PATH];
+        if (!pluginBundle) {
+            NSLog(@"TotalFinderInjector: Unable to load bundle from path: %@", TOTALFINDER_PLUGIN_PATH);
+            return 2;
+        }
+        TotalFinderPlugin* principalClass = (TotalFinderPlugin*)[pluginBundle principalClass];
+        if (!principalClass) {
+            NSLog(@"TotalFinderInjector: Unable to retrieve principalClass for bundle: %@", pluginBundle);
+            return 3;
+        }
+        if ([principalClass respondsToSelector:@selector(install)]) {
+            NSLog(@"TotalFinderInjector: Installing TotalFinder ...");
+            [principalClass install];
+        }
+        alreadyLoaded = true;
+        return noErr;
+    } @catch (NSException* exception) {
+        NSLog(@"TotalFinderInjector: Failed to load TotalFinder with exception: %@", exception);
+    }
+    return 1;
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# TotalFinder.osax
+
+This source code implements scripting additions used by TotalFinder.
+
+**TotalFinder** is a plugin for Apples's Finder.app which brings tabs, dual panels and more!
+
+<img src="http://totalfinder.binaryage.com/shared/img/totalfinder-mainshot.png">
+
+## Is this replacement for SIMBL?
+
+Yes, this is SIMBL lite tailored specifically for TotalFinder.
+
+You may want to read the article about my motivations:
+[http://blog.binaryage.com/totalfinder-without-simbl](http://blog.binaryage.com/totalfinder-without-simbl)
+
+
+## Visit [totalfinder.binaryage.com](http://totalfinder.binaryage.com)
+
+#### License: [MIT-Style](totalfinder-kext/raw/master/license.txt)
