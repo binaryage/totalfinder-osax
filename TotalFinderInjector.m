4182ca2f8c42958225feec345f7b84a55e5b6c34
Antonin Hildebrand <antonin@hildebrand.cz>
2011-03-22 08:34:33 +0100

    bump max safe Finder version to 10.6.8

--- TotalFinderInjector.m
+++ TotalFinderInjector.m
@@ -2,7 +2,7 @@
 
 #define TOTALFINDER_STANDARD_INSTALL_LOCATION @"/Applications/TotalFinder.app"
 #define FINDER_MIN_TESTED_VERSION @"10.6"
-#define FINDER_MAX_TESTED_VERSION @"10.6.7"
+#define FINDER_MAX_TESTED_VERSION @"10.6.8"
 
 // SIMBL-compatible interface
 @interface TotalFinderPlugin: NSObject { 
